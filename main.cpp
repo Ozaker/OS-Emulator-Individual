01876cd30aa0b93b5292945ecada8b8104dfd67a
chris <christianast14@gmail.com>
2024-09-13 18:39:51 +0800

    Fixed formatting and colors

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ string ASCII =
     .append(" | |    | (___ | |  | | |__) | |__  | (___  \\ \\_/ / \n")
     .append(" | |     \\___ \\| |  | |  ___/|  __|  \\___ \\  \\   /  \n")
     .append(" | |____ ____) | |__| | |    | |____ ____) |  | |   \n")
-    .append("  \\_____|_____/ \\____/|_|    |______|_____/   |_|   \n");
+    .append("  \\_____|_____/ \\____/|_|    |______|_____/   |_|   ");
 
 vector<string> validCommands = vector<string>({
     "initialize",
@@ -25,7 +25,8 @@ vector<string> validCommands = vector<string>({
 string takeUserInput() {
     string input;
     while (true) {
-        cout << "~!: ";
+        //cout << "~!: ";
+        cout << "Enter a command: ";
         cin >> input;
 
         // check if valid comands contains input
@@ -52,7 +53,7 @@ bool executeCommand(string input) {
 }
 
 int main() {
-    cout << ASCII + '\n';
+    cout << ASCII + '\n' + "\033[32mHello, Welcome to CSOPESY commandline!\033[0m" + '\n' + "\033[93mType 'exit' to quit, 'clear' to clear the screen\033[0m" + '\n';
 
     bool stop = false;
     while (!stop) {
