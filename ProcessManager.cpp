6916dd3606d3d9d24efe302ac27b9ee9c3d36727
svcheng <samuel.vincent.cheng@gmail.com>
2024-09-20 18:01:34 +0800

    transferred some logic in ProcessManager main loop to separate functions

--- ProcessManager.cpp
+++ ProcessManager.cpp
@@ -14,86 +14,97 @@ const static string mainHeader =
 
 
 ProcessManager::ProcessManager() 
-	: initialized(false), mainConsole("main_process", mainHeader, "Enter a command: "), attachedConsole(&this->mainConsole) {}
+	: initialized(false), mainConsole("main_process", mainHeader, "Enter a command: "), attachedConsole(&this->mainConsole), mainAttached(true) {}
 
 
 void ProcessManager::start() {
-	bool mainInControl = true;
 	bool hasExited = false;
 
 	this->attachedConsole->show();
 	while (!hasExited) {
-		hasExited = mainLoopInner(mainInControl);
+		hasExited = mainLoopInner();
 	}	
 }
 
 
-bool ProcessManager::mainLoopInner(bool& mainInControl) {
+bool ProcessManager::mainLoopInner() {
 	string input;
 	this->attachedConsole->takeInput(input);
 	vector<string> command = Utils::parseCommand(input);
 
-	if (mainInControl && command[0] == "clear") {
+	if (this->mainAttached && command[0] == "clear") {
 		this->attachedConsole->clear();
 	}
-	else if (mainInControl && command[0] == "exit") {
+	else if (this->mainAttached && command[0] == "exit") {
 		return true;
 	}
-	else if (mainInControl && command[0] == "initialize") {
+	else if (this->mainAttached && command[0] == "initialize") {
 		if (!this->initialized) {
 			this->config.loadFromFile("config.txt");
 		}
 		this->attachedConsole->print(command[0] + " command recognized. Doing something.\n");
 	}
-	else if (mainInControl && command[0] == "screen") {
-		if (command[1] == "-s") {
-			auto [newConsole, exists] = tryAddConsole(command[2]);
-			if (exists) {
-				this->attachedConsole->print("A screen with the same name already exists. Choose a unique name.\n");
-			}
-			else {
-				this->attachedConsole = newConsole;
-				mainInControl = false;
-				this->attachedConsole->show();
-			}
-		}
-		else if (command[1] == "-r") {
-			auto it = this->consoles.find(command[2]);
-			// if does not exist
-			if (it == this->consoles.end()) { 
-				this->attachedConsole->print("No screen with name " + command[2] + " exists.\n");
-			}
-			else {
-				this->attachedConsole = &it->second;
-				mainInControl = false;
-				this->attachedConsole->show();
-			}
-		} 
-		else if (command[1] == "-ls") {
-			for (const auto& [name, console] : this->consoles) {
-				this->attachedConsole->print("Process: " + name + '\n');
-			}
-		}
-		else {
-			this->attachedConsole->print("Invalid arguments for screen command.\n");
-		}
+	else if (this->mainAttached && command[0] == "screen") {
+		executeScreenCommand(command);
 	}
-	else if (mainInControl && command[0] == "scheduler-test" || command[0] == "scheduler-stop" || command[0] == "report-util") {
+	else if (this->mainAttached && command[0] == "scheduler-test" || command[0] == "scheduler-stop" || command[0] == "report-util") {
 		this->attachedConsole->print(command[0] + " command recognized. Doing something.\n");
 	}
-	else if (!mainInControl && command[0] == "process-smi") {
-		this->attachedConsole->print("Placeholder.\n");
-	}
-	else if (!mainInControl && command[0] == "exit") {
-		this->attachedConsole = &this->mainConsole;
-		mainInControl = true;
-		this->attachedConsole->show();
+	else if (!this->mainAttached && command[0] == "process-smi" || command[0] == "exit") {
+		executeSubscreenCommand(command);
 	}
 	else if (!command[0].empty()) {
 		this->attachedConsole->print(command[0] + " command does not exist.\n");
 	}
 
 	return false;
+}
+
+
+void ProcessManager::executeScreenCommand(vector<string> command) {
+	if (command[1] == "-s") {
+		auto [newConsole, exists] = tryAddConsole(command[2]);
+		if (exists) {
+			this->attachedConsole->print("A screen with the same name already exists. Choose a unique name.\n");
+		}
+		else {
+			this->attachedConsole = newConsole;
+			this->mainAttached = false;
+			this->attachedConsole->show();
+		}
+	}
+	else if (command[1] == "-r") {
+		auto it = this->consoles.find(command[2]);
+		// if does not exist
+		if (it == this->consoles.end()) {
+			this->attachedConsole->print("No screen with name " + command[2] + " exists.\n");
+		}
+		else {
+			this->attachedConsole = &it->second;
+			this->mainAttached = false;
+			this->attachedConsole->show();
+		}
+	}
+	else if (command[1] == "-ls") {
+		for (const auto& [name, console] : this->consoles) {
+			this->attachedConsole->print("Process: " + name + '\n');
+		}
+	}
+	else {
+		this->attachedConsole->print("Invalid arguments for screen command.\n");
+	}
+}
+
+
+void ProcessManager::executeSubscreenCommand(const vector<string> command) {
+	if (command[0] == "process-smi") {
+		this->attachedConsole->print("Placeholder.\n");
+	}
+	else if (command[0] == "exit") {
+		this->attachedConsole = &this->mainConsole;
+		this->mainAttached = true;
+		this->attachedConsole->show();
+	}
 }
 
 
